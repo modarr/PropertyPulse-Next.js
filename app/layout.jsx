f90e219f34562dcc95f6602b8d6a69bd27023c85
Modar Hamdan <modar.hamdan@georgebrown.ca>
2024-06-19 13:44:52 -0400

    next js project setup

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "@/assets/styles/globals.css";
+export const metadata = {
+  title: "PropertyPulse | Find Perfect Rental",
+  description: "Find your dream property",
+  keywords: "rental , find rentals, find properties",
+};
+
+const mainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>;
+      </body>
+    </html>
+  );
+};
+
+export default mainLayout;
