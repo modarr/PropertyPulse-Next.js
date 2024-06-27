ba1d0164f9d4049cc19470cec74b2f660f6d9d46
Modar Hamdan <modar.hamdan@georgebrown.ca>
2024-06-27 13:47:46 -0400

    route init

---
+++ app/api/properties/route.jsx
@@ -0,0 +1,11 @@
+import connectDB from "@/config/database";
+
+export const GET = async (request) => {
+  try {
+    await connectDB();
+    return new Response("hello world", { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("something went wrong", { status: 500 });
+  }
+};
