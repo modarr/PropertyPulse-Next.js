1e95155ea99bac9c414c6cc8dd423067b11ef169
Modar Hamdan <modar.hamdan@georgebrown.ca>
2024-07-06 14:54:35 -0400

    save user to database and session

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,3 +1,5 @@
+import connectDB from "@/config/database";
+import User from "@/models/User";
 import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   providers: [
@@ -18,16 +20,34 @@ export const authOptions = {
 
     async signIn({ profile }) {
       // connect to database
+      await connectDB();
+
       // check if user exists
+      const userExist = await User.findOne({ email: profile.email });
       // if not, add user to database
+      if (!userExist) {
+        // truncate username if too long
+        const username = profile.name.slice();
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.image,
+        });
+      }
+
       // return true to allow the sign in
+      return true;
     },
     // modify the session obj
 
     async session({ session }) {
       // get user from database
+      const user = await User.findOne({ email: session.user.email });
       // assign the user id to the session
+      session.user.id = user._id.toString();
       // return session
+      return session;
     },
   },
 };
