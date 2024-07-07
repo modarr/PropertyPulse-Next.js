bb5ce7421b21bbbc4fbae4e2890dc4c9dbd97c71
Modar Hamdan <modar.hamdan@georgebrown.ca>
2024-07-07 13:14:51 -0400

    signout and protect routes with middleware

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -191,6 +191,9 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-0"
+                      onClick={() => {
+                        setIsProfileMenueOpen(false);
+                      }}
                     >
                       Your Profile
                     </Link>
@@ -200,10 +203,17 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-2"
+                      onClick={() => {
+                        setIsProfileMenueOpen(false);
+                      }}
                     >
                       Saved Properties
                     </Link>
                     <button
+                      onClick={() => {
+                        setIsProfileMenueOpen(false);
+                        signOut();
+                      }}
                       className="block px-4 py-2 text-sm text-gray-700"
                       role="menuitem"
                       tabIndex="-1"
